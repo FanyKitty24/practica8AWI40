bdea282ae1289602a774afb45a9747c0ee0a5e2d
FanyKitty24 <fanysalaslabrada@gmail.com>
2025-03-06 12:11:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,8 @@ def app2():
     con.close()
 
     return "<h5>Hola, soy Fany B)</h5>"
-
+    
+# Asistencias
 @app.route("/asistencias")
 def asistencias():
     if not con.is_connected():
@@ -77,133 +78,68 @@ def asistencias():
 
     return render_template("asistencias.html", asistencias=registros)
 
-@app.route("/asistencias/buscar", methods=["GET"])
-def buscarAsistencias():
+# Empleados
+@app.route("/empleados")
+def empleados():
     if not con.is_connected():
         con.reconnect()
 
-    args     = request.args
-    busqueda = args["busqueda"]
-    busqueda = f"%{busqueda}%"
-    
     cursor = con.cursor(dictionary=True)
     sql    = """
-    SELECT idAsistencia,
-           idEmpleado,
-           idReporte,
-           estado
+    SELECT idEmpleado,
+           nombreEmpleado,
+           numero,
+           fechaIngreso
 
-    FROM asistencias
-
-    WHERE idEmpleado LIKE %s
-    OR    idReporte          LIKE %s
-    OR    estado     LIKE %s
-
-    ORDER BY idAsistencia DESC
+    FROM empleados
 
     LIMIT 10 OFFSET 0
     """
-    val    = (busqueda, busqueda, busqueda)
 
-    try:
-        cursor.execute(sql, val)
-        registros = cursor.fetchall()
-
-        # Si manejas fechas y horas
-        """
-        for registro in registros:
-            fecha_hora = registro["Fecha_Hora"]
-
-            registro["Fecha_Hora"] = fecha_hora.strftime("%Y-%m-%d %H:%M:%S")
-            registro["Fecha"]      = fecha_hora.strftime("%d/%m/%Y")
-            registro["Hora"]       = fecha_hora.strftime("%H:%M:%S")
-        """
-
-    except mysql.connector.errors.ProgrammingError as error:
-        print(f"Ocurrió un error de programación en MySQL: {error}")
-        registros = []
-
-    finally:
-        con.close()
-
-    return make_response(jsonify(registros))
-
-@app.route("/asistencia", methods=["POST"])
-# Usar cuando solo se quiera usar CORS en rutas específicas
-# @cross_origin()
-def guardarAsistencia():
-    if not con.is_connected():
-        con.reconnect()
-
-    id          = request.form["id"]
-    empleado      = request.form["empleado"]
-    reporte      = request.form["reporte"]
-    estado = request.form["estado"]
-    # fechahora   = datetime.datetime.now(pytz.timezone("America/Matamoros"))
-    
-    cursor = con.cursor()
-
-    if id:
-        sql = """
-        UPDATE asistencias
-
-        SET idEmpleado = %s,
-            idReporte      = %s,
-            estado    = %s
-
-        WHERE idAsistencia = %s
-        """
-        val = (empleado, reporte, estado, id)
-    else:
-        sql = """
-        INSERT INTO productos (Nombre_Producto, Precio, Existencias)
-                    VALUES    (%s,          %s,      %s)
-        """
-        val =                 (nombre, precio, existencias)
-    
-    cursor.execute(sql, val)
-    con.commit()
-    con.close()
-
-    return make_response(jsonify({}))
-
-@app.route("/asistencia/<int:id>")
-def editarAsistencia(id):
-    if not con.is_connected():
-        con.reconnect()
-
-    cursor = con.cursor(dictionary=True)
-    sql    = """
-    SELECT idAsistencia, idEmpleado, idReporte, estado
-
-    FROM asistencias
-
-    WHERE idAsistencia = %s
-    """
-    val    = (id,)
-
-    cursor.execute(sql, val)
+    cursor.execute(sql)
     registros = cursor.fetchall()
-    con.close()
 
-    return make_response(jsonify(registros))
+    # Si manejas fechas y horas
+    """
+    for registro in registros:
+        fecha_hora = registro["Fecha_Hora"]
 
-@app.route("/asistencia/eliminar", methods=["POST"])
-def eliminarAsistencia():
+        registro["Fecha_Hora"] = fecha_hora.strftime("%Y-%m-%d %H:%M:%S")
+        registro["Fecha"]      = fecha_hora.strftime("%d/%m/%Y")
+        registro["Hora"]       = fecha_hora.strftime("%H:%M:%S")
+    """
+
+    return render_template("empleados.html", empleados=registros)
+
+
+#Reportes
+@app.route("/reportes")
+def reportes():
     if not con.is_connected():
         con.reconnect()
 
-    id = request.form["id"]
-
     cursor = con.cursor(dictionary=True)
     sql    = """
-    DELETE FROM asistencias
-    WHERE idAsistencia = %s
+    SELECT idReporte,
+           fecha,
+           comentarios
+
+    FROM reportes
+
+    LIMIT 10 OFFSET 0
     """
-    val    = (id,)
 
-    cursor.execute(sql, val)
-    con.commit()
-    con.close()
+    cursor.execute(sql)
+    registros = cursor.fetchall()
 
-    return make_response(jsonify({}))
+    # Si manejas fechas y horas
+    """
+    for registro in registros:
+        fecha_hora = registro["Fecha_Hora"]
+
+        registro["Fecha_Hora"] = fecha_hora.strftime("%Y-%m-%d %H:%M:%S")
+        registro["Fecha"]      = fecha_hora.strftime("%d/%m/%Y")
+        registro["Hora"]       = fecha_hora.strftime("%H:%M:%S")
+    """
+
+    return render_template("reportes.html", reportes=registros)
